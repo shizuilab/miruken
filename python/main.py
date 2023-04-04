7f5a1add80d1eb089b5eb29658089d961ff01371
shizuilab <shizuilab@gmail.com>
2023-04-04 15:56:40 +0900

    back to 2.7inch and added system_unit_files

--- python/main.py
+++ python/main.py
@@ -22,18 +22,22 @@ import cv2
 import numpy as np
 
 import time
-from waveshare_epd import epd2in13_V3 #e-Paperごとに適合する物に置き換える
+from waveshare_epd import epd2in7 #e-Paperごとに適合する物に置き換える
+# from waveshare_epd import epd2in13_V3 #e-Paperごとに適合する物に置き換える
 import logging
 import qrcode
 
-epd = epd2in13_V3.EPD() #e-Paperごとに適合する物に置き換える
-print (epd2in13_V3.EPD_WIDTH, epd2in13_V3.EPD_HEIGHT) #e-Paperごとに適合する物に置き換える
+epd = epd2in7.EPD() #e-Paperごとに適合する物に置き換える
+print (epd2in7.EPD_WIDTH, epd2in7.EPD_HEIGHT) #e-Paperごとに適合する物に置き換える
+# epd = epd2in13_V3.EPD() #e-Paperごとに適合する物に置き換える
+# print (epd2in13_V3.EPD_WIDTH, epd2in13_V3.EPD_HEIGHT) #e-Paperごとに適合する物に置き換える
 
 if len(sys.argv) < 3:
   print("Usage: argv erro")
   sys.exit(1)
 
-images = ['shizui_2in13.bmp', 'shizui_2in13blank.bmp'] #e-Paperごとに適合する物に置き換える
+images = ['shizui.bmp', 'blank.bmp'] #e-Paperごとに適合する物に置き換える
+#images = ['shizui_2in13.bmp', 'shizui_2in13blank.bmp'] #e-Paperごとに適合する物に置き換える
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -51,7 +55,8 @@ print (address)
 def img_add_msg(img, message):
 
     # テキストの描画位置を指定
-    text_x, text_y = 10, 74 #e-Paperごとに適合する座標に置き換える
+    text_x, text_y = 10, 30 #e-Paperごとに適合する座標に置き換える
+    #text_x, text_y = 10, 74 #e-Paperごとに適合する座標に置き換える
     # フォントの指定
     font20 = ImageFont.truetype(os.path.join(picdir, 'NotoSansCJK-Regular.ttc'), 20)
     img = Image.fromarray(img)                          # cv2(NumPy)型の画像をPIL型に変換
@@ -93,7 +98,8 @@ def img_add_msg(img, message):
     return img
 
 # 画像の読み込み
-img = cv2.imread(os.path.join(picdir, 'shizui_2in13background.bmp'))
+img = cv2.imread(os.path.join(picdir, 'shizui_v.bmp')) #e-Paperごとに適合する位置に置き換える
+# img = cv2.imread(os.path.join(picdir, 'shizui_2in13background.bmp')) #e-Paperごとに適合する位置に置き換える
 print (img.shape[0], img.shape[1])
 
 # QRコードの生成
@@ -107,7 +113,9 @@ qr_width, qr_height = qr_img.shape[:2]
 
 # QRコードの配置
 qr_x = img.shape[1] - qr_height - 10 #e-Paperごとに適合する位置に置き換える
-qr_y = img.shape[2] - qr_width - 84 #e-Paperごとに適合する位置に置き換える
+qr_y = img.shape[2] - qr_width - 10 #e-Paperごとに適合する位置に置き換える
+# qr_x = img.shape[1] - qr_height - 10 #e-Paperごとに適合する位置に置き換える
+# qr_y = img.shape[2] - qr_width - 84 #e-Paperごとに適合する位置に置き換える
 img[qr_y:qr_y+qr_height, qr_x:qr_x+qr_width] = qr_img
 
 # 画像に文字を入れる関数を実行
@@ -115,15 +123,16 @@ out_put = img_add_msg(img, text)
 print (out_put.shape)
 
 # 画像の移動、回転と保存 e-Paperごとに適合するように調整する
-center_location = (124, 124)
-angle = 270
-M = cv2.getRotationMatrix2D(center_location, angle, 1)
-out_put = cv2.warpAffine(out_put, M, (out_put.shape[0], out_put.shape[1]))
-print (out_put.shape)
-M2 = np.float32([[1,0,0],[0,1,0]])
-out_put = cv2.warpAffine(out_put, M2, (out_put.shape[1], out_put.shape[0]))
-print (out_put.shape)
-cv2.imwrite(os.path.join(picdir, 'output.bmp'), out_put[0:250, 64:186])
+# center_location = (124, 124)
+# angle = 270
+# M = cv2.getRotationMatrix2D(center_location, angle, 1)
+# out_put = cv2.warpAffine(out_put, M, (out_put.shape[0], out_put.shape[1]))
+# print (out_put.shape)
+# M2 = np.float32([[1,0,0],[0,1,0]])
+# out_put = cv2.warpAffine(out_put, M2, (out_put.shape[1], out_put.shape[0]))
+# print (out_put.shape)
+# cv2.imwrite(os.path.join(picdir, 'output.bmp'), out_put[0:250, 64:186])
+cv2.imwrite(os.path.join(picdir, 'output.bmp'), out_put)
 
 # 音声の再生
 subprocess.Popen(['mpg321', os.path.join(voicedir) + '/src_views_resources_audio_ding.mp3'])
